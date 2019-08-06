0bd45975180787fcc13c8d155c8d4beca8a4ebb3
María Gabriela <mg.rdza25@hotmail.com>
2019-08-06 09:55:04 -0600

    Menu component

--- test-gamanza/src/App.js
+++ test-gamanza/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Container } from 'react-bootstrap';
+import Menu from './components/Menu/Menu';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
-    </div>
+    <Menu />
   );
 }
 
---
+++ test-gamanza/src/components/Menu/Menu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Navbar, Nav } from 'react-bootstrap';
+
+const Menu = props => {
+    return(
+        <Navbar bg="dark" variant="dark">
+            <Nav className="mr-auto">
+                <Nav.Link href="#home">HOME</Nav.Link>
+                <Nav.Link href="#episodes">EPISODES</Nav.Link>
+            </Nav>
+        </Navbar>
+    );
+}
+
+export default Menu;
